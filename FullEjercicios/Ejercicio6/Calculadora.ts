f98a3e3aeca4bc1ae691abc77c8e055bd0cb6be5
David Barraud <davidbarraud@gmail.com>
2025-05-08 22:21:46 -0300

    Ejercicios terminados

--- FullEjercicios/Ejercicio6/Calculadora.ts
+++ FullEjercicios/Ejercicio6/Calculadora.ts
@@ -21,24 +21,42 @@ class Calculadora {
         return this.resultado;
     }
 */
-    public suma(num1:number, num2:number): void {
-        this.resultado = num1 + num2;
-        console.log (`La Suma del número: ${num1} y el número ${num2} es: ${this.resultado} \n`);
+
+//Métods para realizar las operaciones
+  private suma(num1:number, num2:number): number {
+       return this.resultado = num1 + num2;
+    }
+public resta(num1:number, num2:number): number {
+       return this.resultado = num1 - num2;
+}
+public multiplica(num1:number, num2:number): number {
+    return this.resultado = num1 * num2;
+       
+}
+public divide(num1:number, num2:number): number {
+    return this.resultado = num1 / num2;
+}
+
+// métodos que implementan las operaciones de la calculadora
+//Métod para Sumar
+    public sumar(num1:number, num2:number): void {
+        const resultado = this.suma(num1, num2);
+        console.log (`La Suma del número: ${num1} y el número ${num2} es: ${resultado} \n`);
     }
 
 // Método para Restar
-    public resta(num1:number, num2:number): void {
-        this.resultado = num1 - num2;
-        console.log (`La Resta del número: ${num1} y el número ${num2} es: ${this.resultado} \n`);
+    public restar(num1:number, num2:number): void {
+        const resultado = this.resta(num1, num2);
+        console.log (`La Resta del número: ${num1} y el número ${num2} es: ${resultado} \n`);
     }
 
-// Método para Multiplicar
+// Método para Multiplicar. valida si hay un cero para no avanzar
 public multiplicar(num1:number, num2:number): void {
     if (num1 === 0 || num2 ===0) {
         console.log (`La multiplicación por cero da siempre cero \n`);   
     } else {
-        this.resultado = num1 * num2;
-        console.log (`La Multiplicación del número: ${num1} y el número ${num2} es: ${this.resultado} \n`);
+        const resultado = this.multiplica(num1, num2);
+        console.log (`La Multiplicación del número: ${num1} y el número ${num2} es: ${resultado} \n`);
     }
 }
 
@@ -47,18 +65,18 @@ public dividir(num1:number, num2:number): void {
     if (num1===0 || num2 ===0) {
         console.log (`No se puede dividir por cero \n`);   
     } else {
-        this.resultado = num1 / num2;
-        console.log (`La División del número: ${num1} y el número ${num2} es: ${this.resultado} \n`);
+        const resultado = this.divide(num1, num2);
+        console.log (`La División del número: ${num1} y el número ${num2} es: ${resultado} \n`);
     }
 }
 
 }
 
 const operacion = new Calculadora;
-operacion.suma(123, 2);
-operacion.suma(100, 10);
-operacion.resta(9, 5);
-operacion.resta(487, 500);
+operacion.sumar(123, 2);
+operacion.sumar(100, 10);
+operacion.restar(9, 5);
+operacion.restar(487, 500);
 operacion.multiplicar(10, 10);
 operacion.multiplicar(0, 10);
 operacion.multiplicar(10, 0);
